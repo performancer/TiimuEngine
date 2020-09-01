b0c09a1720ae9660835aa91c1b4264867fcaaa12
latte1635 <lauri.lundell@outlook.com>
2020-09-01 21:42:51 +0300

    refactor: casting struct variables

--- engine/texture.cpp
+++ engine/texture.cpp
@@ -29,7 +29,7 @@ struct texture texture_load(const char* path)
 
 	free(image);
 
-	struct texture t = { texture_handle, width, height };
+	struct texture t = { texture_handle, (unsigned short)width, (unsigned short)height };
 	return t;
 }
 
