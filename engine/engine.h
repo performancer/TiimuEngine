7892903429600d8a3a584e82e1bd6d1e26c44bd0
Alex <alex.leppakoski@hotmail.com>
2020-09-03 20:51:58 +0200

    refactor: abstracting engine

--- engine/engine.h
+++ engine/engine.h
@@ -1,20 +1,27 @@
 #pragma once
-
 #include "vector.h"
 #include "rectangle.h"
 #include "texture.h"
 #include "render_target.h"
 #include "shader.h"
 #include "graphics.h"
-
 #include "input.h"
 #include "keys.h"
-
 #include "frame_counter.h"
-
 #include "audio/audio.h"
 
-void engine_initialize(int width, int height, const char* title);
-void engine_run(void(*update)(double), void(*draw)(double));
-void engine_resize_viewport(int width, int height);
-struct vector engine_monitor_resolution();
+class engine
+{
+private:
+	void _do_initialize(int width, int height, const char* title);
+	void _do_update(double delta);
+	void _do_draw(double delta);
+	void _cleanup();
+public:
+	void run(int width, int height, const char* title);
+	virtual void initialize() = 0;
+	virtual void load_content() = 0;
+	virtual void unload_content() = 0;
+	virtual void update(double delta) = 0;
+	virtual void draw(double delta) = 0;
+};
